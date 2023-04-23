dc270bd1f57d2935f60ff22a996ba0d70b050d70
Andreu-G <andreugonzalezm@gmail.com>
2023-04-23 02:53:28 +0200

    changed project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'yt-downloader'`, () => {
+  it(`should have as title 'dragonrain'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('yt-downloader');
+    expect(app.title).toEqual('dragonrain');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('yt-downloader app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('dragonrain app is running!');
   });
 });
